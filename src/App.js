1023632fe32b04cef06cba88607e3a6cd1a94005
Sabir <gadimovsabir@outlook.com>
2023-08-17 10:06:40 +0400

    temp

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
 import logo from './logo.svg';
+import { useEffect, useState } from "react";
+import jwt_decode from 'jwt-decode';
 import './App.css';
 
+
+// dont forget console.log 
+
 function App() {
+
+  const [ user, setUser ] = useState({});
+
+  function handleCallbackResponse(response) {
+    var token = response.credential
+    console.log("Encoded JWT ID Token: " + token);
+    var userObject = jwt_decode(token);
+    console.log(userObject);
+    setUser(userObject);
+    document.getElementById("signInDiv").hidden = true;
+  }
+
+  function handleSignOut(event) {
+    setUser({});
+    document.getElementById("signInDiv").hidden = false;
+  }
+
+  useEffect(() => {
+    /*global google*/
+    // change credentials 
+    google.accounts.id.initialize({
+      client_id: "749035568078-vusb1fvn9vutcfnrgllad36hmko8tvea.apps.googleusercontent.com",
+      callback: handleCallbackResponse
+    });
+
+    google.accounts.id.renderButton(
+      document.getElementById("signInDiv"),
+      { theme: "outline", size: "large"}
+    );
+  }, [])
+
+    // OPTIONAL CODE BELOW - USE IF YOU WANT TO PROPT A LOG IN ON PAGE LOAD.
+    //google.accounts.id.prompt();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id='signInDiv'></div>
+      { Object.keys(user).length != 0 &&
+        <button onClick={ (e)  => handleSignOut(e)}>Sign Out</button>
+      }
+      
+      { user &&
+        <div>
+          <img src={user.picture}></img>
+          <h3>{user.name}</h3>
+        </div>
+      }
     </div>
   );
 }
