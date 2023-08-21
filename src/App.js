ca9790e9f352e2c3c15986086753f6755725070d
Sabir <gadimovsabir@outlook.com>
2023-08-21 10:10:52 +0400

    add msm page styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import logo from './logo.svg';
 import { useEffect, useState } from "react";
 import jwt_decode from 'jwt-decode';
+import MSM from "./assets/msm.png"
 import './App.css';
 
 
@@ -8,13 +8,11 @@ import './App.css';
 
 function App() {
 
-  const [ user, setUser ] = useState({});
+  const [ user, setUser ] = useState([]);
 
   function handleCallbackResponse(response) {
     var token = response.credential
-    console.log("Encoded JWT ID Token: " + token);
     var userObject = jwt_decode(token);
-    console.log(userObject);
     setUser(userObject);
     document.getElementById("signInDiv").hidden = true;
   }
@@ -25,8 +23,6 @@ function App() {
   }
 
   useEffect(() => {
-    /*global google*/
-    // change credentials 
     google.accounts.id.initialize({
       client_id: "749035568078-vusb1fvn9vutcfnrgllad36hmko8tvea.apps.googleusercontent.com",
       callback: handleCallbackResponse
@@ -38,23 +34,44 @@ function App() {
     );
   }, [])
 
-    // OPTIONAL CODE BELOW - USE IF YOU WANT TO PROPT A LOG IN ON PAGE LOAD.
-    //google.accounts.id.prompt();
+
 
   return (
+    <>
+    <div id='signInDiv'></div>
     <div className="App">
-      <div id='signInDiv'></div>
-      { Object.keys(user).length != 0 &&
-        <button onClick={ (e)  => handleSignOut(e)}>Sign Out</button>
-      }
-      
-      { user &&
-        <div>
-          <img src={user.picture}></img>
-          <h3>{user.name}</h3>
-        </div>
-      }
-    </div>
+    {
+  Object.keys(user).length != 0 && 
+  <>
+<div className="App-Header">
+  <div className="App-Header-Left">
+    <div><img src={MSM}></img></div>
+    <h1>SMS Göndəriş</h1>
+    <h1> Hesabatlar</h1>
+    <h1> Qruplar</h1>
+    <h1> Hazır mesajlar</h1>
+    <h1>Qara siyahı</h1>
+    <h1> Admin</h1>
+  </div>  
+  <div className="App-Header-Right">
+    
+    {Object.keys(user).length != 0 && <>
+      <h1>{user.name}</h1>
+    <button onClick={ (e)  => handleSignOut(e)}>Çıxış</button>
+    </>  }
+    
+  </div>
+</div>
+<div className="App-Footer">
+  <div> <h1>MSMT LLC Mobile Solutions & Marketing</h1> <p>© 2023 - Bütün hüquqları qorunur.</p> </div>
+  <div><h1>Kaveroçkin küç 127-129 m 8 AZ1007 Bakı</h1> <p>Telefon: (+994 12) 441 55 11</p> </div>
+  <div><h1>info@msm.az</h1> <p>Texniki dəstək : (+994 12) 441 55 11</p> </div>
+</div>
+  </>
+  }
+</div>
+
+</>
   );
 }
 
