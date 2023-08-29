53ca9c4d66cf01a7b13b7e1cd195c45cb225b149
perto57 <ogulcanerim57@gmail.com>
2023-08-29 03:02:46 +0300

    custom button logic
    custom msm page log in button added
    main logic implemented
    little change left to be made

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import jwt_decode from 'jwt-decode';
 import MSM from "./assets/msm.png"
 import './App.css';
 
-
+/* global google */
 // dont forget console.log 
 
 function App() {
 
-  const [ user, setUser ] = useState([]);
+  const [user, setUser] = useState([]);
 
   function handleCallbackResponse(response) {
     var token = response.credential
@@ -28,50 +28,41 @@ function App() {
       callback: handleCallbackResponse
     });
 
-    google.accounts.id.renderButton(
-      document.getElementById("signInDiv"),
-      { theme: "outline", size: "large"}
-    );
-  }, [])
-
-
+    google.accounts.id.disableAutoSelect();
+  }, []);
 
   return (
     <>
-    <div id='signInDiv'></div>
-    <div className="App">
-    {
-  Object.keys(user).length != 0 && 
-  <>
-<div className="App-Header">
-  <div className="App-Header-Left">
-    <div><img src={MSM}></img></div>
-    <h1>SMS Göndəriş</h1>
-    <h1> Hesabatlar</h1>
-    <h1> Qruplar</h1>
-    <h1> Hazır mesajlar</h1>
-    <h1>Qara siyahı</h1>
-    <h1> Admin</h1>
-  </div>  
-  <div className="App-Header-Right">
-    
-    {Object.keys(user).length != 0 && <>
-      <h1>{user.name}</h1>
-    <button onClick={ (e)  => handleSignOut(e)}>Çıxış</button>
-    </>  }
-    
-  </div>
-</div>
-<div className="App-Footer">
-  <div> <h1>MSMT LLC Mobile Solutions & Marketing</h1> <p>© 2023 - Bütün hüquqları qorunur.</p> </div>
-  <div><h1>Kaveroçkin küç 127-129 m 8 AZ1007 Bakı</h1> <p>Telefon: (+994 12) 441 55 11</p> </div>
-  <div><h1>info@msm.az</h1> <p>Texniki dəstək : (+994 12) 441 55 11</p> </div>
-</div>
-  </>
-  }
-</div>
-
-</>
+      <div id='signInDiv'></div>
+      <div className="App">
+        <div className="App-Header">
+          <div className="App-Header-Left">
+            <div><img src={MSM}></img></div>
+            <h1>SMS Göndəriş</h1>
+            <h1> Hesabatlar</h1>
+            <h1> Qruplar</h1>
+            <h1> Hazır mesajlar</h1>
+            <h1>Qara siyahı</h1>
+            <h1> Admin</h1>
+          </div>  
+          <div className="App-Header-Right">
+            {Object.keys(user).length !== 0 ? (
+              <>
+                <h1>{user.name}</h1>
+                <button onClick={(e) => handleSignOut(e)}>Çıxış</button>
+              </>
+            ) : (
+              <button onClick={() => google.accounts.id.prompt()}>Log In</button>
+            )}
+          </div>
+        </div>
+        <div className="App-Footer">
+          <div> <h1>MSMT LLC Mobile Solutions & Marketing</h1> <p>© 2023 - Bütün hüquqları qorunur.</p> </div>
+          <div><h1>Kaveroçkin küç 127-129 m 8 AZ1007 Bakı</h1> <p>Telefon: (+994 12) 441 55 11</p> </div>
+          <div><h1>info@msm.az</h1> <p>Texniki dəstək : (+994 12) 441 55 11</p> </div>
+        </div>
+      </div>
+    </>
   );
 }
 
